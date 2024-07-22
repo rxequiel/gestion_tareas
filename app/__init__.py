f9f0483afc80388730efbccbcee859ff927c77b4
rxequiel <delahoz27montes@gmail.com>
2024-07-22 15:56:53 -0500

    Se añaden las configuraciones de la base de datos y realizan las primeras migraciones con modelos user y task, tambien se modifica el archivo __init__.py creando create_app() para inicializacion y configuracion de dependencias por ultimo se modifica archivo run.py para lanzamiento de la app

---
+++ app/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+
+db = SQLAlchemy()
+migrate = Migrate()
+jwt = JWTManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
+
+    with app.app_context():
+        from app import models
+    """"
+    from .routes import main_bp
+    from .auth import auth_bp
+
+    app.register_blueprint(main_bp)
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+    """
+
+    return app
---
+++ app/models.py
@@ -0,0 +1,16 @@
+from . import db
+from datetime import datetime
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    tasks = db.relationship('Task', backref='user', lazy=True)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+    done = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
